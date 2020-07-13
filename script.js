6354719952542785f5a2383b0e799f6f211a7bd3
Шама <Shamilllaa@mail.ru>
2020-07-13 18:05:36 +0700

    'promiseComplicated'

--- script.js
+++ script.js
@@ -1,56 +1,43 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
 
-const output = document.getElementById('output');
+    const select = document.getElementById('cars'),
+        output = document.getElementById('output');
 
-const getData = (url) => {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.open('GET', url);
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState !== 4) {
-                return;
-            }
-            if (request.status === 200) {
-                const response = JSON.parse(request.responseText);
-                resolve(response);
-            } else {
-                reject(request.statusText);
+    const getOutput = () => {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('GET', './cars.json');
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState !== 4) {
+                    return;
+                } 
+                if (request.status === 200) {
+                    const response = JSON.parse(request.responseText);
+                    resolve(response);
+                } else {
+                    reject(request.statusText);
+                }
+            });
+            request.send();
+        });
+    };
+
+    const outputData = (data) => {
+        data.cars.forEach(item => {
+            if (item.brand === select.value) {
+                const {brand, model, price} = item;
+                    output.innerHTML = `Тачка ${brand} ${model} <br>
+                        Цена: ${price}$`;
             }
         });
-        request.send();
+    };
+
+    select.addEventListener('change', () => {
+        getOutput()
+        .then(outputData)
+        .catch(error => output.innerHTML = 'Произошла ошибка');
     });
- 
-};
-const outputPhoto = (data) => {
-    data.forEach(item => {
-        output.insertAdjacentHTML('beforebegin', 
-        `<h4>${item.title}</h4>
-        <img src='${item.thumbnailUrl}' alt='${item.title}'>`);
-    });
-};
 
-const urlPhotos = 'https://jsonplaceholder.typicode.com/photos';
-
-const oneImg = getData('https://jsonplaceholder.typicode.com/photos/1'),
-    twoImg = getData('https://jsonplaceholder.typicode.com/photos/2'),
-    threeImg = getData('https://jsonplaceholder.typicode.com/photos/5');
-
-// getData(urlPhotos)
-// .then(outputPhoto)
-// .catch(error => console.error(error));
-
-// oneImg
-// .then(outputPhoto)
-// .catch(error => console.error(error));
-
-// twoImg
-// .then(outputPhoto)
-// .catch(error => console.error(error));
-
-// Promise.race([oneImg, twoImg])
-// .then(outputPhoto)
-// .catch(error => console.error(error));
-
-Promise.all([oneImg, threeImg, twoImg])
-.then(outputPhoto)
-.catch(error => console.error(error));
+});
